885f7db38f2d66d37b47fc4380b03e32f3266f1e
Alexey Gerasev <alexey.gerasev@gmail.com>
2017-06-21 17:46:22 +0700

    complete task5

--- task5/task5.cc
+++ task5/task5.cc
@@ -21,20 +21,20 @@ void task5() {
 
   factory->SetInputTrees(tin, "pid==0", "pid==1");
 
-  factory->AddVariable("el0 := elayer[0]",'D');
-  factory->AddVariable("el1 := elayer[1]",'D');
-  factory->AddVariable("el2 := elayer[2]",'D');
-  factory->AddVariable("el3 := elayer[3]",'D');
-  factory->AddVariable("el4 := elayer[4]",'D');
-  factory->AddVariable("el5 := elayer[5]",'D');
-  factory->AddVariable("el6 := elayer[6]",'D');
-  factory->AddVariable("el7 := elayer[7]",'D');
-  factory->AddVariable("el8 := elayer[8]",'D');
-  factory->AddVariable("el9 := elayer[9]",'D');
+  factory->AddVariable("el0 := elayer[0]",'F');
+  factory->AddVariable("el1 := elayer[1]",'F');
+  factory->AddVariable("el2 := elayer[2]",'F');
+  factory->AddVariable("el3 := elayer[3]",'F');
+  factory->AddVariable("el4 := elayer[4]",'F');
+  factory->AddVariable("el5 := elayer[5]",'F');
+  factory->AddVariable("el6 := elayer[6]",'F');
+  factory->AddVariable("el7 := elayer[7]",'F');
+  factory->AddVariable("el8 := elayer[8]",'F');
+  factory->AddVariable("el9 := elayer[9]",'F');
 
   factory->PrepareTrainingAndTestTree("","");
 
-  //factory->BookMethod(TMVA::Types::kLikelihood, "Likelihood", "H:!V:VarTransform=N,D"); // :PDFInterpol=Spline2:TransformOutput=True
+  //factory->BookMethod(TMVA::Types::kLikelihood, "Likelihood", "H:!V:VarTransform=N,D:PDFInterpol=Spline2:TransformOutput=True");
   //factory->BookMethod(TMVA::Types::kPDERS, "PDERS", "H:!V:VolumeRangeMode=RMS:KernelEstimator=Gauss");
   factory->BookMethod(TMVA::Types::kBDT, "BDT", "H:!V:NTrees=800:MaxDepth=5:SeparationType=GiniIndex:BoostType=AdaBoost");
 
--- task5/task5read.cc
+++ task5/task5read.cc
@@ -3,6 +3,14 @@
 
 using namespace TMVA;
 
+char digit[] = "0123456789";
+
+void book(Reader *reader, const char *method) {
+  std::string path(method);
+  path = "weights/myTMVA_" + path + ".weights.xml";
+  reader->BookMVA(method, path.c_str());
+}
+
 void task5read() {
 
   gSystem->Load("libTMVA");
@@ -12,7 +20,7 @@ void task5read() {
   TChain *tin = new TChain("tree");
   tin->Add("tree.root");
 
-  int pid; double esum, elayer[10];
+  int pid; float esum, elayer[10];
   tin->GetBranch("pid")->SetAddress(&pid);
   tin->GetBranch("esum")->SetAddress(&esum);
   tin->GetBranch("elayer")->SetAddress(elayer);
@@ -21,51 +29,72 @@ void task5read() {
   for( int i=0; i<10; i++ ) 
     reader->AddVariable(Form("elayer[%d]",i),&(el[i]));
   
-  const char *method = "BDT";
-  std::string path(method);
-  path = "weights/myTMVA_" + path + ".weights.xml";
-  reader->BookMVA(method, path.c_str());
-
-  double l = -0.2, r = 0.4;
-  int nbin = 100;
-  TH1D *h0 = new TH1D("h0","h0",nbin,l,r);
-  TH1D *h1 = new TH1D("h1","h1",nbin,l,r);
-
-  for( int i=0; i<tin->GetEntries(); i++ ) {
-    tin->GetEvent(i);
-    if ( pid != 0 && pid != 1 ) continue;
-    for( int j=0; j<10; j++ ) el[j]=elayer[j];
-    double out = reader->EvaluateMVA(method);
-    if( pid==0 ) h0->Fill(out);
-    else h1->Fill(out);
-  }
-
   TCanvas *c = new TCanvas("c","",900,600);
-  c->Divide(2,1);
-
-  h0->SetLineColor(kRed);
-  h0->SetFillStyle(3003);
-  h0->SetFillColor(kRed);
-
-  h1->SetLineColor(kBlue);
-  h1->SetFillColor(kBlue);
-  h1->SetFillStyle(3008);
-
-  c->cd(1);
-  h0->Draw();
-  h1->Draw("same");
-
-  double *x = new double[nbin], *y = new double[nbin];
-  int csig = 0, cbg = 0;
-  int nsig = h0->GetEntries(), nbg = h1->GetEntries();
-  for (int i = 0; i < nbin; ++i) {
-    csig += h0->GetBinContent(i);
-    cbg += h1->GetBinContent(i);
-    x[i] = double(csig)/nsig;
-    y[i] = double(cbg)/nbg;
+  c->Divide(2,2);
+  
+  int nbin = 100;
+  double b[6] = {-3.0, 3.0, -0.5, 1.5, -0.5, 0.5};
+  TH1D *hist[6];
+  for (int i = 0; i < 3; ++i) {
+    std::string name;
+    name = std::string("sig") + digit[i];
+    hist[2*i] = new TH1D(name.c_str(),name.c_str(),nbin,b[2*i],b[2*i + 1]);
+    name = std::string("bg") + digit[i];
+    hist[2*i + 1] = new TH1D(name.c_str(),name.c_str(),nbin,b[2*i],b[2*i + 1]);
   }
   
-  c->cd(2);
-  TGraph *graph = new TGraph(nbin,x,y);
-  graph->Draw("AL*");
+  const char *methods[] = {"Likelihood", "PDERS", "BDT"};
+  for (int type = 0; type < 3; ++type) {
+    const char *method = methods[type];
+    book(reader, method);
+
+    for( int i=0; i<tin->GetEntries(); i++ ) {
+      tin->GetEvent(i);
+      if ( pid != 0 && pid != 1 ) continue;
+      for( int j=0; j<10; j++ ) el[j]=elayer[j];
+      double out = reader->EvaluateMVA(method);
+      if( pid==0 ) hist[2*type]->Fill(out);
+      else hist[2*type + 1]->Fill(out);
+      //printf("%f\n", out);
+    }
+
+    hist[2*type]->SetLineColor(kRed);
+    hist[2*type]->SetFillStyle(3003);
+    hist[2*type]->SetFillColor(kRed);
+
+    hist[2*type + 1]->SetLineColor(kBlue);
+    hist[2*type + 1]->SetFillColor(kBlue);
+    hist[2*type + 1]->SetFillStyle(3008);
+
+    c->cd(type + 1);
+    hist[2*type]->Draw();
+    hist[2*type + 1]->Draw("same");
+  }
+
+  c->cd(4);
+  TGraph *graph[3];
+  for (int type = 0; type < 3; ++type) {
+    double *x = new double[nbin], *y = new double[nbin];
+    int csig = 0, cbg = 0;
+    int nsig = hist[2*type]->GetEntries();
+    int nbg = hist[2*type + 1]->GetEntries();
+    for (int i = 0; i < nbin; ++i) {
+      csig += hist[2*type]->GetBinContent(i);
+      cbg += hist[2*type + 1]->GetBinContent(i);
+      x[i] = double(csig)/nsig;
+      y[i] = double(cbg)/nbg;
+    }
+    
+    graph[type] = new TGraph(nbin,x,y);
+    int color[] = {kRed, kBlue, kGreen};
+    graph[type]->SetLineColor(color[type]);
+    graph[type]->SetMarkerColor(color[type]);
+    if(type == 0) {
+      graph[type]->Draw("AL*");
+    } else {
+      graph[type]->Draw("LP*");
+    }
+    delete[] x;
+    delete[] y;
+  }
 }
